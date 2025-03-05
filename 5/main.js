352cfc1cf2f994757d8be596743304bc68d74a4a
bacnx <bacnx@outfiz.com>
2025-03-05 16:03:23 +0700

    Done lession5

---
+++ 5/main.js
@@ -0,0 +1,18 @@
+// alert("hello");
+// prompt("What is your name?");
+
+// khai báo biến
+var mayName;
+let YourName;
+
+//Khởi tạo giá trị cho biến
+myName = "Nguyễn Xuân Bắc";
+yourName = "Nguyen Van B";
+const gioiTinh = "Nam";
+
+console.log("Tên tôi là: " + myName);
+console.log(yourName);
+
+// Bài tập nhập tên vào prompt và in
+let inputName = prompt("Nhập tên của bạn: ");
+alert("Xin chào " + inputName);
